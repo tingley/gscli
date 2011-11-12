15c2d7025d7b1bd283bd93572805e5c883c6758a
tingley <chase@spartanconsultiginc.com>
2011-11-12 14:35:00 -0800

    Add new unittests to AllTests

--- test/com/globalsight/tools/gscli/AllTests.java
+++ test/com/globalsight/tools/gscli/AllTests.java
@@ -10,7 +10,9 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
     TestFileProfilesParser.class,
     TestJobsParser.class,
-    TestTaskParser.class
+    TestTaskParser.class,
+    TestActivityTypesParser.class,
+    TestProjectsParser.class
 })
 public class AllTests {
     public static void main(String[] args) {
